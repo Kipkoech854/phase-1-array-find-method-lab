dc509cc5b7dd5a59d6eb82a2acbcc5a4ad995a3b
Gideon kipkoech <gideon.kipkoech1@student.moringaschool.com>
2025-03-25 12:58:52 +0300

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    console.log("Record received:", record); 
+    let win = record.find(season => season.result === "W");
+    console.log("Winning season found:", win); 
+    return win ? win.year : undefined;
+}
+
+
+    
+    
